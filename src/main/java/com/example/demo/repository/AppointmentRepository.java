768d868ba8fcb8412c84754d15812c1ae7aec11d
Hadzija <hadzijasale@gmail.com>
2021-01-24 15:51:14 +0100

    #Appointment repository and service.

---
+++ src/main/java/com/example/demo/repository/AppointmentRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
+}
---
+++ src/main/java/com/example/demo/service/AppointmentService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Appointment;
+
+import java.util.List;
+
+public interface AppointmentService {
+    Appointment findById(Integer id);
+}
