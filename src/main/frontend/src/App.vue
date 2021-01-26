add90ed4ab06da9e9ca8531b91a9e40552759855
Hadzija <hadzijasale@gmail.com>
2021-01-26 14:34:28 +0100

    frontend setup

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavigationBar/>
+  <router-view/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavigationBar from './components/NavigationBar.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    NavigationBar
   }
 }
 </script>
@@ -21,6 +21,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+/* #nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+} */
 </style>
---
+++ src/main/frontend/src/views/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ProfileMenu/>
+    </div>
+</template>
+
+<script>
+import ProfileMenu from '../components/ProfileMenu.vue'
+export default {
+    name: 'Profile',
+    components: {
+        ProfileMenu,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
