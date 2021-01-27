b9cac3fb98303ab37a25e726b8f1864d252184ce
lemara98 <mile.knezevic98@gmail.com>
2021-01-27 16:26:53 +0100

    Map added to home page

---
+++ src/main/frontend/src/textual_components/home/Introduction.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>
+    Welcome to Pharmacy system Home Page
+  </h2>
+  <h3>
+    Pharmacy system lorem ipsum...
+  </h3>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
--- src/main/frontend/src/views/Home.vue
+++ src/main/frontend/src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="sidenav">
+    <h1>
+      <Introduction />
+    </h1>
+    <div class="panel-map">
+      <MapContainer/>
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MapContainer from "@/components/MapContainer";
+import Introduction from "@/textual_components/home/Introduction";
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    MapContainer,
+    Introduction
   }
+
 }
 </script>
+
+<style>
+
+.sidenav {
+  padding-top: 20px;
+  background-color: #f1f1f1;
+  height: 100%;
+}
+
+.panel-map {
+  padding-left: 20px;
+  padding-right: 20px;
+  background-color: #42b983;
+}
+
+</style>
