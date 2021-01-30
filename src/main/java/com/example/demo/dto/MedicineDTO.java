f524b32ed94e8fc4ee9dcf4182c2f52c663ff026
Ana Atanackovic <anaatanackovic98@gmail.com>
2021-01-30 19:18:23 +0100

    Added data transfer objects for medicine needs

---
+++ src/main/java/com/example/demo/dto/MedicineDTO.java
@@ -0,0 +1,73 @@
+package com.example.demo.dto;
+
+import com.example.demo.model.Medicine;
+
+public class MedicineDTO 
+{
+    private long id;
+    private String name;
+    private boolean recipeNeed;
+    private MedicineManufacturerDTO medicineManufacturer;
+    private MedicineShapeDTO medicineShape;
+    
+	public MedicineDTO() {
+	}
+	
+	public MedicineDTO(Medicine medicine) {
+		this.id = medicine.getId();
+		this.name = medicine.getName();
+		this.recipeNeed = medicine.isRecipeNeeded();
+		this.medicineManufacturer = new MedicineManufacturerDTO(medicine.getMedicineManufacturer());
+		this.medicineShape = new MedicineShapeDTO(medicine.getMedicineShape());
+	}
+	
+	public MedicineDTO(long id, String name, boolean recipeNeed, MedicineManufacturerDTO medicineManufacturer,
+			MedicineShapeDTO medicineShape) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.recipeNeed = recipeNeed;
+		this.medicineManufacturer = medicineManufacturer;
+		this.medicineShape = medicineShape;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public boolean isRecipeNeed() {
+		return recipeNeed;
+	}
+	
+	public void setRecipeNeed(boolean recipeNeed) {
+		this.recipeNeed = recipeNeed;
+	}
+	
+	public MedicineManufacturerDTO getMedicineManufacturer() {
+		return medicineManufacturer;
+	}
+	
+	public void setMedicineManufacturer(MedicineManufacturerDTO medicineManufacturer) {
+		this.medicineManufacturer = medicineManufacturer;
+	}
+	
+	public MedicineShapeDTO getMedicineShape() {
+		return medicineShape;
+	}
+	
+	public void setMedicineShape(MedicineShapeDTO medicineShape) {
+		this.medicineShape = medicineShape;
+	}
+}
---
+++ src/main/java/com/example/demo/dto/MedicineManufacturerDTO.java
@@ -0,0 +1,38 @@
+package com.example.demo.dto;
+
+import com.example.demo.model.MedicineManufacturer;
+
+public class MedicineManufacturerDTO 
+{
+	private long id;
+    private String name;
+    
+	public MedicineManufacturerDTO() {
+	}
+	
+	public MedicineManufacturerDTO(MedicineManufacturer medicineManufacturer) {
+		this(medicineManufacturer.getId(), medicineManufacturer.getName());
+	}
+
+	public MedicineManufacturerDTO(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
