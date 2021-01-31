0df4a1f584380c75586a502d97be3d75dc28a5e5
Ana Atanackovic <anaatanackovic98@gmail.com>
2021-01-31 12:59:40 +0100

    Added service methods for appointment

--- src/main/java/com/example/demo/service/AppointmentService.java
+++ src/main/java/com/example/demo/service/AppointmentService.java
@@ -1,7 +1,28 @@
 package com.example.demo.service;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.example.demo.model.Appointment;
 
 public interface AppointmentService {
-    Appointment findById(Long id);
+	
+	Appointment findOne(Long id);
+
+	List<Appointment> findAll();
+	
+	Page<Appointment> findAll(Pageable page);
+	
+	List<Appointment> findAllByStartTime(Timestamp startTime);
+
+	List<Appointment> findAllByEndTime(Timestamp endTime);
+	
+	List<Appointment> findAllByPrice(double price);
+
+	Appointment save(Appointment appointment);
+
+	void remove(Long id);
 }
