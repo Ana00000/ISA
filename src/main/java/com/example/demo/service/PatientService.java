c7227d6db7af8ec466376c3e2be82fd1c74b1ee0
Ana Atanackovic <anaatanackovic98@gmail.com>
2021-01-31 14:16:33 +0100

    Added service methods for patient

---
+++ src/main/java/com/example/demo/service/PatientService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import com.example.demo.model.Patient;
+
+public interface PatientService {
+	
+	Patient findOne(Long id);
+	    
+	Patient findOneByPassword(String password);
+
+	List<Patient> findAll();
+	
+	Page<Patient> findAll(Pageable page);
+	
+	List<Patient> findAllByName(String name);
+	
+	List<Patient> findAllByLastName(String lastName);
+	
+	List<Patient> findByNameAndLastNameAllIgnoringCase(String name, String lastName);
+	
+	List<Patient> findAllByEmail(String email);
+
+	Patient save(Patient patient);
+
+	void remove(Long id);
+}
---
+++ src/main/java/com/example/demo/service/impl/PatientServiceImpl.java
@@ -0,0 +1,61 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import com.example.demo.model.Patient;
+import com.example.demo.repository.PatientRepository;
+import com.example.demo.service.PatientService;
+
+@Service
+public class PatientServiceImpl implements PatientService {
+
+    private PatientRepository patientRepository;
+    
+	@Autowired
+	public PatientServiceImpl(PatientRepository patientRepository) {
+		this.patientRepository = patientRepository;
+	}
+    
+    public Patient findOne(Long id) {
+		return patientRepository.findById(id).orElseGet(null);
+	}
+    
+    public Patient findOneByPassword(String password) {
+		return patientRepository.findOneByPassword(password);
+	}
+
+	public List<Patient> findAll() {
+		return patientRepository.findAll();
+	}
+	
+	public Page<Patient> findAll(Pageable page) {
+		return patientRepository.findAll(page);
+	}
+	
+	public List<Patient> findAllByName(String name) {
+		return patientRepository.findAllByName(name);
+	}
+	
+	public List<Patient> findAllByLastName(String lastName) {
+		return patientRepository.findAllByLastName(lastName);
+	}
+	
+	public List<Patient> findByNameAndLastNameAllIgnoringCase(String name, String lastName) {
+		return patientRepository.findByNameAndLastNameAllIgnoringCase(name, lastName);
+	}
+	
+	public List<Patient> findAllByEmail(String email) {
+		return patientRepository.findAllByEmail(email);
+	}
+
+	public Patient save(Patient patient) {
+		return patientRepository.save(patient);
+	}
+
+	public void remove(Long id) {
+		patientRepository.deleteById(id);
+	}
+}
