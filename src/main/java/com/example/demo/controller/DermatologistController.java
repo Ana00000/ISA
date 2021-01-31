46fc7498646174b80cab1742e85e5f28d585c49d
Ana Atanackovic <anaatanackovic98@gmail.com>
2021-01-31 20:39:43 +0100

    Added controller for dermatologists, get all returns nulls (needs to be resolved)

---
+++ src/main/java/com/example/demo/controller/DermatologistController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.DermatologistDTO;
+import com.example.demo.model.Dermatologist;
+import com.example.demo.service.DermatologistService;
+
+@RestController
+@RequestMapping(value = "/dermatologists", produces = MediaType.APPLICATION_JSON_VALUE)
+public class DermatologistController {
+	
+	private DermatologistService dermatologistService;
+    
+	@Autowired
+	public DermatologistController(DermatologistService dermatologistService) {
+		this.dermatologistService = dermatologistService;
+	}
+	
+	@GetMapping(value = "/all")
+	public ResponseEntity<List<DermatologistDTO>> getAllDermatologists() {
+
+		List<Dermatologist> dermatologists = dermatologistService.findAll();
+
+		List<DermatologistDTO> dermatologistsDTO = new ArrayList<>();
+		for (Dermatologist d : dermatologists) {
+			dermatologistsDTO.add(new DermatologistDTO(d));
+		}
+
+		return new ResponseEntity<>(dermatologistsDTO, HttpStatus.OK);
+	}
+}
