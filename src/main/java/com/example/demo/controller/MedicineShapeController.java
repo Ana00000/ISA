848fc60ded0e3f41797dceaa4c88da5ec6bb6090
Ana Atanackovic <anaatanackovic98@gmail.com>
2021-01-31 20:10:53 +0100

    Added controller for medicine shape (getAll tested on postman)

---
+++ src/main/java/com/example/demo/controller/MedicineShapeController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.MedicineShapeDTO;
+import com.example.demo.model.MedicineShape;
+import com.example.demo.service.MedicineShapeService;
+
+import org.springframework.http.MediaType;
+
+@RestController
+@RequestMapping(value = "/medicineShape", produces = MediaType.APPLICATION_JSON_VALUE)
+public class MedicineShapeController {
+	
+	private MedicineShapeService medicineShapeService;
+    
+	@Autowired
+	public MedicineShapeController(MedicineShapeService medicineShapeService) {
+		this.medicineShapeService = medicineShapeService;
+	}
+	
+	@GetMapping(value = "/all")
+	public ResponseEntity<List<MedicineShapeDTO>> getAllMedicineShapes() {
+
+		List<MedicineShape> medicineShapes = medicineShapeService.findAll();
+
+		List<MedicineShapeDTO> medicineShapesDTO = new ArrayList<>();
+		for (MedicineShape ms : medicineShapes) {
+			medicineShapesDTO.add(new MedicineShapeDTO(ms));
+		}
+
+		return new ResponseEntity<>(medicineShapesDTO, HttpStatus.OK);
+	}
+}
