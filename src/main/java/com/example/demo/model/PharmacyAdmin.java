dfcfd1f25e1fbf156135a5223fd845d953e6ac0b
hadzija <hadzijasale@gmail.com>
2021-02-02 09:35:01 +0100

    #pom changed

--- src/main/java/com/example/demo/model/PharmacyAdmin.java
+++ src/main/java/com/example/demo/model/PharmacyAdmin.java
@@ -7,39 +7,13 @@ import java.util.List;
 import java.util.Objects;
 
 public class PharmacyAdmin extends User{
-    @OneToMany( cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    List<Pharmacy> pharmacies;
 
     public PharmacyAdmin() {
         super();
     }
 
-    public List<Pharmacy> getPharmacies() {
-        return pharmacies;
-    }
-
-    public void setPharmacies(List<Pharmacy> pharmacies) {
-        this.pharmacies = pharmacies;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        PharmacyAdmin that = (PharmacyAdmin) o;
-        return Objects.equals(pharmacies, that.pharmacies);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), pharmacies);
-    }
-
     @Override
     public String toString() {
-        return "PharmacyAdmin{" +
-                "pharmacies=" + pharmacies +
-                '}';
+        return "PharmacyAdmin{}";
     }
 }
