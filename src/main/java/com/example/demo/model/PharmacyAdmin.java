40bb727f70d8eb402258abe899fe110b0602c2f1
hadzija <hadzijasale@gmail.com>
2021-02-02 10:05:29 +0100

    #removed tomcat-starter from pom

--- src/main/java/com/example/demo/model/PharmacyAdmin.java
+++ src/main/java/com/example/demo/model/PharmacyAdmin.java
@@ -1,11 +1,15 @@
 package com.example.demo.model;
 
+import jdk.jfr.Enabled;
+
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import java.util.List;
 import java.util.Objects;
 
+@Entity
 public class PharmacyAdmin extends User{
 
     public PharmacyAdmin() {
