48b9fe650cdb4c4f5c62f670e865c5d89a288a15
Ana Atanackovic <anaatanackovic98@gmail.com>
2021-02-03 02:05:08 +0100

    Done get and getAll for appointments of the clients

--- src/main/java/com/example/demo/controller/AppointmentController.java
+++ src/main/java/com/example/demo/controller/AppointmentController.java
@@ -1,10 +1,18 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.AppointmentDTO;
+import com.example.demo.model.Appointment;
 import com.example.demo.service.AppointmentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 @RestController
@@ -17,5 +25,29 @@ public class AppointmentController {
 	public AppointmentController(AppointmentService appointmentService) {
 		this.appointmentService = appointmentService;
 	}
+	
+	@GetMapping(value = "/all")
+	public ResponseEntity<List<AppointmentDTO>> getAllAppointments() {
 
+		List<Appointment> appointments = appointmentService.findAll();
+
+		List<AppointmentDTO> appointmentsDTO = new ArrayList<>();
+		for (Appointment a : appointments) {
+			appointmentsDTO.add(new AppointmentDTO(a));
+		}
+
+		return new ResponseEntity<>(appointmentsDTO, HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<AppointmentDTO> getAppointment(@PathVariable Long id) {
+
+		Appointment appointment = appointmentService.findOne(id);
+
+		if (appointment == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<>(new AppointmentDTO(appointment), HttpStatus.OK);
+	}
 }
