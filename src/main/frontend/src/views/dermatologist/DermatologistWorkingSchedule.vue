2fd0ee7cad6456b2787ac8783dcd0b020f6d9f4b
Ana Atanackovic <anaatanackovic98@gmail.com>
2021-02-03 21:26:14 +0100

    Working schedule for pharmacist (day, week, month, year) and year for dermatologist

--- src/main/frontend/src/views/dermatologist/DermatologistWorkingSchedule.vue
+++ src/main/frontend/src/views/dermatologist/DermatologistWorkingSchedule.vue
@@ -3,7 +3,7 @@
         <div>
             <WorkingSchedule/>
         </div> 
-        <div class="panelDiv">
+        <div>
             <DermatologistMenu/>
         </div> 
     </div>  
--- src/main/frontend/src/views/pharmacist/PharmacistWorkingSchedule.vue
+++ src/main/frontend/src/views/pharmacist/PharmacistWorkingSchedule.vue
@@ -1,35 +1,22 @@
 <template>
-    <div style="
-        background: #13077d;
-        background: -webkit-linear-gradient(to right, #5442ed, #cdc8fa, #13077d);
-        background: linear-gradient(to right, #5442ed, #cdc8fa, #13077d);">
-        <br/>
-        <div class="welcoming">Pharmacist you can find your working schedule here!</div>
-        <div class="panelDiv">
-                <PharmacistMenu/>
+    <div>
+        <div>
+            <WorkingSchedule/>
+        </div> 
+        <div>
+            <PharmacistMenu/>
         </div> 
     </div>  
 </template>
 
 <script>
+import WorkingSchedule from '../../components/WorkingSchedule.vue'
 import PharmacistMenu from '../../components/pharmacist/PharmacistMenu.vue'
 export default {
     name: 'PharmacistWorkingSchedule',    
     components : {   
+        WorkingSchedule,
         PharmacistMenu
     }
 }
 </script>
-
-
-<style scoped>
-.welcoming {
-    font-weight: bolder;
-    font-size: 25px;
-    height: 816px;
-}
-
-.panelDiv {
-    float: bottom;
-}
-</style>
