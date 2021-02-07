575acafc6c03e73cb31906f1231c2858373a965f
hadzija <hadzijasale@gmail.com>
2021-02-07 11:48:46 +0100

    #consultation with pharmacist view

--- src/main/java/com/example/demo/dto/PatientDTO.java
+++ src/main/java/com/example/demo/dto/PatientDTO.java
@@ -1,15 +1,26 @@
 package com.example.demo.dto;
 
+import com.example.demo.model.Patient;
 import com.example.demo.model.User;
 
 import java.io.Serializable;
 
 public class PatientDTO extends UserDTO implements Serializable {
+	private Long penalties;
 
 	public PatientDTO() {
 	}
 
-	public PatientDTO(User user) {
-		super(user);
+	public PatientDTO(Patient patient) {
+		super(patient);
+		this.penalties = patient.getPenalties();
+	}
+
+	public Long getPenalties() {
+		return penalties;
+	}
+
+	public void setPenalties(Long penalties) {
+		this.penalties = penalties;
 	}
 }
